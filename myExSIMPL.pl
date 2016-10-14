2d7846410da2edf689299cacc50f686918f6ceb1
jimmylin192 <linxin@gmail.com>
2016-10-13 19:15:26 -0500

    some eval successful

--- myExSIMPL.pl
+++ myExSIMPL.pl
@@ -1,4 +1,4 @@
-parse(TokenList, AST) :- phrase(program(AST), TokenList, []).
+parse(TokenList, AST) :- phrase(program(AST), TokenList).
 /* Keyword Declarations */
 reserved([var, return, function]).          % reserved words
 operators(['<-', '.', ';', '(', ')', '{', '}']). % assign, statement terminator, parentheses
@@ -16,28 +16,28 @@ statement(assignment(NAME, VALUE)) --> identity(NAME), ['<-'], base(VALUE).
 statement(decAssignment(NAME, VALUE)) --> [var], identity(NAME), ['<-'], base(VALUE).
 
 statement(funcDecl(FUNC_NAME, FUNC_ARGS, FUNC_PROGRAM)) --> 
-              [function], identity(FUNC_NAME), ['('], FUNC_ARGS, [')'],  % function signature
-              ['{'], program(FUNC_PROGRAM), ['}'].                       % function body
+              ['function'], identity(FUNC_NAME), ['('], identity(FUNC_ARGS), [')'],  
+              ['{'], program(FUNC_PROGRAM), ['}'].                                  
 statement(conditional(COND, IF_BRANCH)) --> 
-              ['if'], condition(COND), ['then'], statementSeq(IF_BRANCH), ['endif'].
+              ['if'], ['('], condition(COND), [')'], ['then'], statementSeq(IF_BRANCH), ['endif'].
 statement(conditional(COND, IF_BRANCH, ELSE_BRANCH)) --> 
-              ['if'], condition(COND), ['then'], statementSeq(IF_BRANCH), 
-                                       ['else'], statementSeq(ELSE_BRANCH), ['endif'].
-statement(loop(COND, LOOP_BODY)) --> ['while'], condition(COND), ['do'], statementSeq(LOOP_BODY), ['done'].
+              ['if'], ['('], condition(COND), [')'], ['then'], statementSeq(IF_BRANCH), 
+                                                     ['else'], statementSeq(ELSE_BRANCH), ['endif'].
+statement(loop(COND, LOOP_BODY)) --> ['while'], ['('], condition(COND), [')'], 
+                                     ['do'], statementSeq(LOOP_BODY), ['done'].
 
-statementSeq(STATEMENT) --> statement(STATEMENT), '.'.
-statementSeq(STATEMENT, REMAINDERS) --> statement(STATEMENT), ';', statementSeq(REMAINDERS).
+statementSeq(sseq(STATEMENT)) --> statement(STATEMENT), ['.'].
+statementSeq(sseq(STATEMENT, REMAINDERS)) --> statement(STATEMENT), [';'], statementSeq(REMAINDERS).
 
-% ambiguous parsing?
-condition(OPR_A, COMP, OPR_B) --> base(OPR_A), comparator(COMP), base(OPR_B).
-condition(COND_A, LOG_OP, COND_B) --> ['('], condition(COND_A), logicals(LOG_OP), condition(COND_B), [')'].
+condition(singcond(OPR_A, COMP, OPR_B)) --> base(OPR_A), comparator(COMP), base(OPR_B).
+condition(mulcond(COND_A, LOG_OP, COND_B)) --> ['('], condition(COND_A), logical(LOG_OP), condition(COND_B), [')'].
 
 comparator(COMP) --> [COMP], { comparators(COMPARATORS), member(COMP, COMPARATORS) }.
-logicals(LOG_OP) --> [LOG_OP], { logOp(LOGIC_OPERATORS), member(LOG_OP, LOGIC_OPERATORS) }. 
+logical(LOG_OP) --> [LOG_OP], { logOp(LOGIC_OPERATORS), member(LOG_OP, LOGIC_OPERATORS) }. 
 
 /* Element-wise rule definition */
 base(NAME) --> identity(NAME) | numerics(NAME) | ['('], expression(NAME), [')'].
-base(FUNC_NAME, FUNC_ARG) --> identity(FUNC_NAME), ['('], base(FUNC_ARG), [')'].  % func call
+base(funcCall(FUNC_NAME, FUNC_ARG)) --> identity(FUNC_NAME), ['('], base(FUNC_ARG), [')'].  % func call
 expression(EXP) --> term(EXP) | term(TERM), ['+'], left_assoc(EXP, TERM, addition) | term(TERM), ['-'], left_assoc(EXP, TERM, subtraction).
 term(TERM) --> factor(TERM) | factor(VALUE), ['*'], left_assoc(TERM, VALUE, multiplication) | factor(VALUE), ['/'], left_assoc(TERM, VALUE, division).
 
@@ -50,7 +50,7 @@ identity(NAME) --> [NAME], {
                              comparators(Comparators), \+ member(NAME, Comparators),
                              condWords(CondWords), \+ member(NAME, CondWords),
                              loopWords(LoopWords), \+ member(NAME, LoopWords),
-                             \+ integer(NAME) , \+ float(NAME)
+                             \+ number(NAME)
                            }.
 numerics(VALUE) --> [VALUE], { integer(VALUE) | float(VALUE) }.
 
@@ -79,25 +79,117 @@ evaluate(AST, Number) :- empty_assoc(VARIABLES), empty_assoc(FUNCTIONS), validat
 % validate an empty program or statement
 validate([], _, VAR, VAR, FUNC, FUNC).
 % validate a literal number as it is
-validate(X, X, VAR, VAR, FUNC, FUNC) :- integer(X), float(X).
+validate(X, X, VAR, VAR, FUNC, FUNC) :- number(X).
 % validate a declared variable
-validate(X, OUTCOME, VAR, VAR, FUNC, FUNC) :- get_assoc(X, VAR, OUTCOME), OUTCOME \== empty.
+validate(X, OUTCOME, VAR, VAR, FUNC, FUNC) :- 
+    get_assoc(X, VAR, OUTCOME), 
+    OUTCOME \== empty.
 % validate all arithmetic computations.
-validate(compute(addition, OPR_A, OPR_B), OUTCOME, VAR, VAR, FUNC, FUNC):- validate(OPR_A, VAL_OPR_A, VAR, VAR, FUNC, FUNC), validate(OPR_B, VAL_OPR_B, VAR, VAR, FUNC, FUNC), OUTCOME is VAL_OPR_A + VAL_OPR_B.
-validate(compute(subtraction, OPR_A, OPR_B), OUTCOME, VAR, VAR, FUNC, FUNC):- validate(OPR_A, VAL_OPR_A, VAR, VAR, FUNC, FUNC), validate(OPR_B, VAL_OPR_B, VAR, VAR, FUNC, FUNC), OUTCOME is VAL_OPR_A - VAL_OPR_B.
-validate(compute(multiplication, OPR_A, OPR_B), OUTCOME, VAR, VAR, FUNC, FUNC):- validate(OPR_A, VAL_OPR_A, VAR, VAR, FUNC, FUNC), validate(OPR_B, VAL_OPR_B, VAR, VAR, FUNC, FUNC), OUTCOME is VAL_OPR_A * VAL_OPR_B.
-validate(compute(division, OPR_A, OPR_B), OUTCOME, VAR, VAR, FUNC, FUNC):- VAL_OPR_B \== 0, validate(OPR_A, VAL_OPR_A, VAR, VAR, FUNC, FUNC), validate(OPR_B, VAL_OPR_B, VAR, VAR, FUNC, FUNC), OUTCOME is VAL_OPR_A / VAL_OPR_B.
+validate(compute(addition, OPR_A, OPR_B), OUTCOME, VAR, VAR, FUNC, FUNC):- 
+    validate(OPR_A, VAL_OPR_A, VAR, VAR, FUNC, FUNC), 
+    validate(OPR_B, VAL_OPR_B, VAR, VAR, FUNC, FUNC), 
+    OUTCOME is VAL_OPR_A + VAL_OPR_B.
+validate(compute(subtraction, OPR_A, OPR_B), OUTCOME, VAR, VAR, FUNC, FUNC):- 
+    validate(OPR_A, VAL_OPR_A, VAR, VAR, FUNC, FUNC), 
+    validate(OPR_B, VAL_OPR_B, VAR, VAR, FUNC, FUNC), 
+    OUTCOME is VAL_OPR_A - VAL_OPR_B.
+validate(compute(multiplication, OPR_A, OPR_B), OUTCOME, VAR, VAR, FUNC, FUNC):- 
+    validate(OPR_A, VAL_OPR_A, VAR, VAR, FUNC, FUNC), 
+    validate(OPR_B, VAL_OPR_B, VAR, VAR, FUNC, FUNC), 
+    OUTCOME is VAL_OPR_A * VAL_OPR_B.
+validate(compute(division, OPR_A, OPR_B), OUTCOME, VAR, VAR, FUNC, FUNC):- 
+    VAL_OPR_B \== 0, 
+    validate(OPR_A, VAL_OPR_A, VAR, VAR, FUNC, FUNC), 
+    validate(OPR_B, VAL_OPR_B, VAR, VAR, FUNC, FUNC), 
+    OUTCOME is VAL_OPR_A / VAL_OPR_B.
+% validate all logical computations
+%validate(mulcond(COND_A, LOG_OP, COND_B), OUTCOME, VAR, VAR, FUNC, FUNC):- 
+%    LOG_OP == ['&&'],
+%    validate(COND_A, VAL_COND_A, VAR, VAR, FUNC, FUNC), 
+%    validate(COND_B, VAL_COND_B, VAR, VAR, FUNC, FUNC), 
+%    OUTCOME is VAL_COND_A * VAL_COND_B.
+%validate(mulcond(COND_A, LOG_OP, COND_B), OUTCOME, VAR, VAR, FUNC, FUNC):- 
+%    LOG_OP == ['||'], 
+%    validate(COND_A, VAL_COND_A, VAR, VAR, FUNC, FUNC), 
+%    validate(COND_B, VAL_COND_B, VAR, VAR, FUNC, FUNC), 
+%    OUTCOME is VAL_COND_A + VAL_COND_B.
+%
+%% validate all comparative computations
+validate(singcond(OPR_A, _, OPR_B), _, VAR, VAR, FUNC, FUNC):- 
+    validate(OPR_A, VAL_OPR_A, VAR, VAR, FUNC, FUNC), 
+    validate(OPR_B, VAL_OPR_B, VAR, VAR, FUNC, FUNC), 
+    VAL_OPR_A > VAL_OPR_B.
+%validate(singcond(OPR_A, COMP_OP, OPR_B), OUTCOME, VAR, VAR, FUNC, FUNC):- 
+%    COMP_OP is ['>='], 
+%    validate(OPR_A, VAL_OPR_A, VAR, VAR, FUNC, FUNC), 
+%    validate(OPR_B, VAL_OPR_B, VAR, VAR, FUNC, FUNC), 
+%    OUTCOME is (VAL_OPR_A >= VAL_OPR_B).
+%validate(singcond(OPR_A, COMP_OP, OPR_B), OUTCOME, VAR, VAR, FUNC, FUNC):- 
+%    COMP_OP is ['<'], 
+%    validate(OPR_A, VAL_OPR_A, VAR, VAR, FUNC, FUNC), 
+%    validate(OPR_B, VAL_OPR_B, VAR, VAR, FUNC, FUNC), 
+%    OUTCOME is (VAL_OPR_A < VAL_OPR_B).
+%validate(singcond(OPR_A, COMP_OP, OPR_B), OUTCOME, VAR, VAR, FUNC, FUNC):- 
+%    COMP_OP is ['<='], 
+%    validate(OPR_A, VAL_OPR_A, VAR, VAR, FUNC, FUNC), 
+%    validate(OPR_B, VAL_OPR_B, VAR, VAR, FUNC, FUNC), 
+%    OUTCOME is (VAL_OPR_A =< VAL_OPR_B).
+%validate(singcond(OPR_A, COMP_OP, OPR_B), OUTCOME, VAR, VAR, FUNC, FUNC):- 
+%    COMP_OP is ['=='], 
+%    validate(OPR_A, VAL_OPR_A, VAR, VAR, FUNC, FUNC), 
+%    validate(OPR_B, VAL_OPR_B, VAR, VAR, FUNC, FUNC), 
+%    OUTCOME is (VAL_OPR_A =:= VAL_OPR_B).
+%validate(singcond(OPR_A, COMP_OP, OPR_B), OUTCOME, VAR, VAR, FUNC, FUNC):- 
+%    COMP_OP == ['!='], 
+%    validate(OPR_A, VAL_OPR_A, VAR, VAR, FUNC, FUNC), 
+%    validate(OPR_B, VAL_OPR_B, VAR, VAR, FUNC, FUNC), 
+%    OUTCOME is (VAL_OPR_A =\= VAL_OPR_B).
 
 % validate the return statement
-validate(retStatement(NAME), OUTCOME, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC) :- validate(NAME, OUTCOME, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC).
+validate(retStatement(VALUE), OUTCOME, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC) :- 
+    validate(VALUE, OUTCOME, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC).
 % validate the declaration statement
-validate(declaration(NAME), _, PRE_VAR, POST_VAR, PRE_FUNC, PRE_FUNC) :- \+ get_assoc(NAME, PRE_VAR, _), put_assoc(NAME, PRE_VAR, empty, POST_VAR).
+validate(declaration(NAME), _, PRE_VAR, POST_VAR, PRE_FUNC, PRE_FUNC) :- 
+    \+ get_assoc(NAME, PRE_VAR, _), 
+    put_assoc(NAME, PRE_VAR, empty, POST_VAR).
 % validate the assignment statement
-validate(assignment(NAME, VALUE), _, PRE_VAR, POST_VAR, PRE_FUNC, PRE_FUNC) :- get_assoc(NAME, PRE_VAR, _), validate(VALUE, OUTCOME, PRE_VAR, _, PRE_FUNC, _), put_assoc(NAME, PRE_VAR, OUTCOME, POST_VAR).
+validate(assignment(NAME, VALUE), _, PRE_VAR, POST_VAR, PRE_FUNC, PRE_FUNC) :- 
+    get_assoc(NAME, PRE_VAR, _), 
+    validate(VALUE, OUTCOME, PRE_VAR, _, PRE_FUNC, _), 
+    put_assoc(NAME, PRE_VAR, OUTCOME, POST_VAR).
 % validate the decAssignment 
-validate(decAssignment(NAME, VALUE), _, PRE_VAR, POST_VAR, PRE_FUNC, PRE_FUNC) :- \+ get_assoc(NAME, PRE_VAR, _), validate(VALUE, OUTCOME, PRE_VAR, _, PRE_FUNC, _), put_assoc(NAME, PRE_VAR, OUTCOME, POST_VAR).
+validate(decAssignment(NAME, VALUE), _, PRE_VAR, POST_VAR, PRE_FUNC, PRE_FUNC) :- 
+    \+ get_assoc(NAME, PRE_VAR, _), 
+    validate(VALUE, OUTCOME, PRE_VAR, _, PRE_FUNC, _), 
+    put_assoc(NAME, PRE_VAR, OUTCOME, POST_VAR).
+% validate the conditional
+validate(conditional(COND, BRANCH_IF), OUTCOME, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC) :- 
+    validate(COND, _, PRE_VAR, PRE_VAR, PRE_FUNC, PRE_FUNC),
+    validate(BRANCH_IF, OUTCOME, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC).
+%validate(conditional(COND, _), _, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC) :- 
+%    \+ validate(COND, _, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC).
+%COND_OUTCOME \== true.
+%validate(conditional(COND, IF_BRANCH, _), OUTCOME, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC) :- 
+%    validate(COND, COND_OUTCOME, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC), 
+%    validate(IF_BRANCH, OUTCOME, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC), 
+%    COND_OUTCOME =:= true. 
+%validate(conditional(COND, _, ELSE_BRANCH), OUTCOME, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC) :- 
+%    validate(COND, COND_OUTCOME, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC), 
+%    validate(ELSE_BRANCH, OUTCOME, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC), 
+%    COND_OUTCOME =\= true.
+
+% validate the loop
+% validate sseq
+validate(sseq(STATEMENT), OUTCOME, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC) :- 
+    validate(STATEMENT, OUTCOME, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC).
+validate(sseq(STATEMENT, REMAINDERS), OUTCOME, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC) :- 
+    validate(STATEMENT, _, PRE_VAR, IME_VAR, PRE_FUNC, IME_FUNC), 
+    validate(REMAINDERS, OUTCOME, IME_VAR, POST_VAR, IME_FUNC, POST_FUNC).
 
 % validate the single prog node
-validate(prog(PROGRAM), OUTCOME, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC) :- validate(PROGRAM, OUTCOME, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC).
+validate(prog(PROGRAM), OUTCOME, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC) :- 
+    validate(PROGRAM, OUTCOME, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC).
 % validate the prog node recursively
-validate(prog(PROGRAM, AST), OUTCOME, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC) :- validate(PROGRAM, _, PRE_VAR, VAR_C, PRE_FUNC, FUNC_C), validate(AST, OUTCOME, VAR_C, POST_VAR, FUNC_C, POST_FUNC).
+validate(prog(PROGRAM, AST), OUTCOME, PRE_VAR, POST_VAR, PRE_FUNC, POST_FUNC) :- 
+    validate(PROGRAM, _, PRE_VAR, VAR_C, PRE_FUNC, FUNC_C), 
+    validate(AST, OUTCOME, VAR_C, POST_VAR, FUNC_C, POST_FUNC).
